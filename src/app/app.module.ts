39f897356c90ee52e98a85903019351326fdf61f
Nameyray <eparis@atlassian.com>
2022-04-17 15:08:59 +0300

    added another navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormSearchComponent } from './components/form-search/form-search.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { DatePipe } from './date.pipe';
+import { RepositoryComponent } from './components/repository/repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FormSearchComponent,
+    MainPageComponent,
+    DatePipe,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
